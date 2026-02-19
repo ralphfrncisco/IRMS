c6b44c01e7484b19aaf8bb82f1ffc714d679f365
ralphfrncisco <ralphfrncisco@gmail.com>
2026-02-20 01:59:55 +0800

    grid responsiveness fix

--- src/components/Expenses/StatsGrid.jsx
+++ src/components/Expenses/StatsGrid.jsx
@@ -38,7 +38,7 @@ function StatsGrid() {
     ];
     
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3">
             {statsCards.map((item, index) => (
                 <div 
                     key={index} 
